dd62369b5bf18bf200b9ea3eed6ee5abd91308d8
rajPadval <97424854+rajPadval@users.noreply.github.com>
2022-12-22 23:46:57 +0530

    After checking

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvoaCNQF2Z0T3rmC0VYP9PjDKLRVOkMwE",
+  authDomain: "prichat-c433e.firebaseapp.com",
+  projectId: "prichat-c433e",
+  storageBucket: "prichat-c433e.appspot.com",
+  messagingSenderId: "690987292005",
+  appId: "1:690987292005:web:fe46b85cb35abf0f0c3d8c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
